6992c5386236befe03f8b47c36872bf81f23fd68
gk <gk@LAPTOP-3J2GVT6H>
2024-01-21 23:00:13 +0530

    msg

---
+++ Bike/src/main/java/com/shakti/Bike/BikeApplication.java
@@ -0,0 +1,14 @@
+package com.shakti.Bike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
